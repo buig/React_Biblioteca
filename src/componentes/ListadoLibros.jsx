1f565889eaaee5c41eeb47b17d888dfd164cf235
Jucagovi <jc.gomez@edu.gva.es>
2024-05-07 09:40:59 +0200

    Sesión 2 - error ListadoLibros.jsx

--- src/componentes/ListadoLibros.jsx
+++ src/componentes/ListadoLibros.jsx
@@ -5,12 +5,13 @@ import coleccion from "../assets/bbdd/biblioteca.json";
 const ListadoLibros = () => {
   return (
     <Fragment>
-      <h2>Listado de libros</h2>
-      {Array.isArray(coleccion.libros) && coleccion.libros.length
-        ? coleccion.libros.map((datos_libro) => {
-            return <Libro key={datos_libro.id} datos={datos_libro} />;
-          })
-        : "No se han encontrado libros."}
+      <section className='seccion'>
+        {Array.isArray(coleccion.libros) && coleccion.libros.length
+          ? coleccion.libros.map((datos_libro) => {
+              return <Libro key={datos_libro.id} datos={datos_libro} />;
+            })
+          : "No se han encontrado libros."}
+      </section>
     </Fragment>
   );
 };
